716571d5a136d4b90e956da8c13f5dcf7ab171e2
Shaun Norris <shaunnorris@Shauns-MacBook.local>
2018-05-05 16:22:46 +0800

    added single stats collector script

---
+++ getstats.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+/usr/bin/python /Users/shaunnorris/minecollect/flycash.py
+/usr/bin/python /Users/shaunnorris/minecollect/ethermine.py
+/usr/bin/python /Users/shaunnorris/minecollect/f2pool.py
+
